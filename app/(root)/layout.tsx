3e4a26e11aaee7c1c5687fc769b84892181c3463
waleedfadl <wwaleedfadl@gmail.com>
2025-04-14 22:05:47 +0200

    adding some of the interview page ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,20 +6,12 @@ import React, { ReactNode } from "react";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
-  if (!isUserAuthenticated) redirect("/sign-in");
+  //if (!isUserAuthenticated) redirect("/sign-in");
   return (
     <div className="root-layout">
       <nav>
-        <Link
-          href="/"
-          className="flex items-center gap-2"
-        >
-          <Image
-            src="/logo.svg"
-            alt="logo"
-            width={38}
-            height={32}
-          />
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
           <h2 className="text-primary-100">Interview Teacher</h2>
         </Link>
       </nav>
--- firebase/admin.ts
+++ firebase/admin.ts
@@ -2,19 +2,19 @@ import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 const initFirebaseAdmin = () => {
-  const apps = getApps();
-  if (!apps.length) {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_PROJECT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
-    });
-  }
-  return {
-    auth: getAuth(),
-    db: getFirestore(),
-  };
+	const apps = getApps();
+	if (!apps.length) {
+		initializeApp({
+			credential: cert({
+				projectId: process.env.FIREBASE_PROJECT_ID,
+				clientEmail: process.env.FIREBASE_Client_EMAIL,
+				privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+			}),
+		});
+	}
+	return {
+		auth: getAuth(),
+		db: getFirestore(),
+	};
 };
 export const { auth, db } = initFirebaseAdmin();
