5969004818489bcec519a65bf96e97431abef318
waleedfadl <wwaleedfadl@gmail.com>
2025-04-15 02:07:43 +0200

    try agian

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -1,22 +1,16 @@
 import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
-import { getRandomInterviewCover } from "@/lib/utils";
+
 import { db } from "@/firebase/admin";
-
-////////////////////////////////////////////////////////////
-
-export async function GET() {
-	return Response.json({ success: true, data: "thank u" }, { status: 200 });
-}
-
-///////////////////////
+import { getRandomInterviewCover } from "@/lib/utils";
 
 export async function POST(request: Request) {
-	const { type, role, level, techstack, amount, uerid } = await request.json();
-	try {
-		const { text: questions } = await generateText({
-			model: google("gemini-2.0-flash-001"),
-			prompt: `Prepare questions for a job interview.
+  const { type, role, level, techstack, amount, userid } = await request.json();
+
+  try {
+    const { text: questions } = await generateText({
+      model: google("gemini-2.0-flash-001"),
+      prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
         The tech stack used in the job is: ${techstack}.
@@ -26,30 +20,32 @@ export async function POST(request: Request) {
         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
         Return the questions formatted like this:
         ["Question 1", "Question 2", "Question 3"]
+        
         Thank you! <3
     `,
-		});
-		const interview = {
-			role,
-			type,
-			level,
-			techstack: techstack.split(","),
-			questions: JSON.parse(questions),
-			userId: userid,
-			finalized: true,
-			coverImage: getRandomInterviewCover(),
-			createdAt: new Date().toISOString(),
-		};
-		await db.collection("interviews").add(interview);
-		return Response.json({ successs: true }, { status: 200 });
-	} catch (error) {
-		console.error(error);
-		return Response.json(
-			{
-				success: false,
-				error: error,
-			},
-			{ status: 500 },
-		);
-	}
+    });
+
+    const interview = {
+      role: role,
+      type: type,
+      level: level,
+      techstack: techstack.split(","),
+      questions: JSON.parse(questions),
+      userId: userid,
+      finalized: true,
+      coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
+
+    await db.collection("interviews").add(interview);
+
+    return Response.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return Response.json({ success: false, error: error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
 }
