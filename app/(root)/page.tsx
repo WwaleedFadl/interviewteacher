06a428040f6d18deb4e1729c8354bf74bc10ce99
waleedfadl <wwaleedfadl@gmail.com>
2025-04-15 21:23:06 +0200

    addig an important part

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,15 @@ import { dummyInterviews } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import {getCurrentUser,getInterviewsByUserId} from '@/lib/actions/auth.action'
 
-const page = () => {
+
+const page = async() => {
+  const user = await  getCurrentUser();
+  const userInterviews = await getInterviewsByUserId(user?.id);
+  const hasPasInterviews = userInterviews?.length > 0;
+
+
   return (
     <>
       <section className="card-cta">
@@ -15,7 +22,7 @@ const page = () => {
             Practice on real interview questions & get instant feedback
           </p>
           <Button asChild className="btn-primary max-sm:w-full">
-            <Link href="/intervidew">Start an Interview</Link>
+            <Link href="/interview">Start an Interview</Link>
           </Button>
         </div>
         <Image
@@ -29,10 +36,15 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section flex flex-row ">
-          {/*   <p>You have&apos;t taken any interviews yet.</p>*/}
-          {dummyInterviews.map((interview) => (
+          {
+            hasPasInterviews ? (
+            userInterviews?.map((interview)=>(
             <InterviewCard {...interview} key={interview.id} />
-          ))}
+            ))
+            ) : (
+                  <p>You haven't taken any interviews yet.</p>
+            )
+          )}
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8">
